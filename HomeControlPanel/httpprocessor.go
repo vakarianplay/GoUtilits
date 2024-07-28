2c828b89e10337236dabedf4a44e8543b3b64d85
Kostya Vakarian <vakarianplay@gmail.com>
2024-07-28 17:26:56 +0900

    Clear httpprocessor code

--- HomeControlPanel/httpprocessor.go
+++ HomeControlPanel/httpprocessor.go
@@ -14,29 +14,29 @@ import (
 func getWLEDState() (int, error) {
 	resp, err := http.Get(fmt.Sprintf("http://%s/json/state", readCfg()[5]))
 	if err != nil {
-		return -1, fmt.Errorf("ошибка запроса: %w", err)
+		return -1, fmt.Errorf("Error request: %w", err)
 	}
 	defer resp.Body.Close()
 
 	if resp.StatusCode != http.StatusOK {
-		return -1, fmt.Errorf("неверный статус код: %d", resp.StatusCode)
+		return -1, fmt.Errorf("Error status: %d", resp.StatusCode)
 	}
 
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		return -1, fmt.Errorf("ошибка чтения тела ответа: %w", err)
+		return -1, fmt.Errorf("Error read answer: %w", err)
 	}
 
 	var data map[string]interface{}
 	err = json.Unmarshal(body, &data)
 	if err != nil {
-		return -1, fmt.Errorf("ошибка разбора JSON: %w", err)
+		return -1, fmt.Errorf("Error parsing JSON: %w", err)
 	}
 
 	on, ok := data["on"].(bool)
 	//fmt.Println(on)
 	if !ok {
-		return -1, fmt.Errorf("не удалось получить значение 'on'")
+		return -1, fmt.Errorf("Don't get value 'on'")
 	}
 
 	if on {
@@ -48,58 +48,45 @@ func getWLEDState() (int, error) {
 func wledOn() error {
 	rand.Seed(time.Now().UnixNano())
 	randomMode := rand.Intn(10) + 1
-
-	// Формируем URL для включения ленты
 	url := fmt.Sprintf("http://%s/win&T=1&A=245&FX=%d", readCfg()[5], randomMode)
-
-	// Выполняем HTTP-запрос GET
 	resp, err := http.Get(url)
 	if err != nil {
-		return fmt.Errorf("ошибка при отправке запроса: %w", err)
+		return fmt.Errorf("Error request send: %w", err)
 	}
 	defer resp.Body.Close()
 
-	// Проверяем статус код ответа
 	if resp.StatusCode != http.StatusOK {
-		return fmt.Errorf("неверный статус код: %d", resp.StatusCode)
+		return fmt.Errorf("Error status: %d", resp.StatusCode)
 	}
-
 	return nil
 }
 
 func wledOff() error {
-	// Формируем URL для выключения ленты
 	url := fmt.Sprintf("http://%s/win&T=0", readCfg()[5])
-
-	// Выполняем HTTP-запрос GET
 	resp, err := http.Get(url)
 	if err != nil {
-		return fmt.Errorf("ошибка при отправке запроса: %w", err)
+		return fmt.Errorf("Error request: %w", err)
 	}
 	defer resp.Body.Close()
 
-	// Проверяем статус код ответа
 	if resp.StatusCode != http.StatusOK {
-		return fmt.Errorf("неверный статус код: %d", resp.StatusCode)
+		return fmt.Errorf("Error status: %d", resp.StatusCode)
 	}
-
 	return nil
 }
 
 func sonoffProcessor(action string) (string, error) {
-
 	url := "http://" + readCfg()[8] + action
-
 	response, err := http.Get(url)
 	if err != nil {
-		log.Fatal("Ошибка при отправке GET-запроса:", err)
+		log.Fatal("Error seng GET:", err)
 	}
 	defer response.Body.Close()
 
 	var buffer bytes.Buffer
 	_, err = buffer.ReadFrom(response.Body)
 	if err != nil {
-		log.Fatal("Ошибка при чтении ответа:", err)
+		log.Fatal("Read Error:", err)
 	}
 
 	ans := buffer.String()
